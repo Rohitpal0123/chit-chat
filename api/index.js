fc77267204ac0b366b9497d9475c815f2592832b
Rohit Pal <rohit.coding.0123@gmail.com>
2023-08-06 13:15:47 +0530

    Karan

--- api/index.js
+++ api/index.js
@@ -1,23 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
-
-mongoose.connect(process.env.MONGO_URL);
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+  })
+);
+const port = process.env.PORT || 4000;
+
+//jwt(json wen token) used for secure transaction of data between two parties
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
 app.get("/test", (req, res) => {
   res.json("Test is working");
 });
 
+//POST api to register User via login credential using npm package "JWT"
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  await User.create({ username, password });
+  const createdUser = await User.create({ username, password });
+
+  jwt.sign({ UserId: createdUser._id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie("token", token).status(201).json("ok");
+  });
 });
 
-const port = process.env.PORT || 4000;
 app.listen(4000, () => {
   console.log(`Server is running on port ${port} ...`);
 });
